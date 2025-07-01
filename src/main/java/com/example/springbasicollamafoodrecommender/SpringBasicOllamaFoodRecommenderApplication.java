9b0a3fcd3c7766bdcc33b2f65a0d8cf5c2fc5ce9
khang0015 <thaidoankhang04@gmail.com>
2025-07-01 15:54:38 +0700

    first commit

---
+++ src/main/java/com/example/springbasicollamafoodrecommender/SpringBasicOllamaFoodRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbasicollamafoodrecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicOllamaFoodRecommenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicOllamaFoodRecommenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbasicollamafoodrecommender/SpringBasicOllamaFoodRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbasicollamafoodrecommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicOllamaFoodRecommenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
